35b9ad79561329ff004c40feed715199bdf0c5e6
javi <java@javamurray.com>
2026-08-31 10:52:24 -0700

    last hyprland Change

--- modules/desktop/hyprland-home.nix
+++ modules/desktop/hyprland-home.nix
@@ -5,17 +5,14 @@
   home.file.".config/hypr/var.lua".source = ./hyprland/var.lua;
 
   home.file.".config/hypr/windows_and_workspaces.lua".source = ./hyprland/windows_and_workspaces.lua;
-  home.file.".config/hypr/wallpaper1.png".source = ./hyprland/wallpaper/wallpaper1.png;
+  home.file.".config/hypr/wallpaper1.png".source = ./wallpaper/wallpaper1.png;
 
-  home.file.".config/kitty/kitty.conf".source = ./hyprland/kitty/kitty.conf;
+  home.file.".config/kitty/kitty.conf".source = ./kitty/kitty.conf;
   wayland.windowManager.hyprland.systemd.enable = false;
 
   # swayosd needs its own service for volume/brightness OSD popups
   services.swayosd.enable = true;
 
-  # Kvantum
-  home.file.".config/Kvantum/kvantum.kvconfig".source = ./hyprland/kvantum/kvantum.kvconfig;
-  home.file.".config/Kvantum/Orchis".source = ./hyprland/kvantum/Orchis;
   ## widget config files 
   # home.file.".config/ags/config.js".source = ./hyprland/ags/config.js;
 
