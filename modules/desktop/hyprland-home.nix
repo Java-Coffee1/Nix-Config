64b0a8b59d70747d8ef37c26076380f8ed881c75
javi <java@javamurray.com>
2026-08-31 10:33:35 -0700

    mush

--- modules/desktop/hyprland-home.nix
+++ modules/desktop/hyprland-home.nix
@@ -13,24 +13,11 @@
   # swayosd needs its own service for volume/brightness OSD popups
   services.swayosd.enable = true;
 
-  ## rofi configuration files
-  home.file.".config/rofi/config.rasi".source = ./hyprland/rofi/config.rasi;
-  home.file.".config/rofi/colors.rasi".source = ./hyprland/rofi/colors.rasi;
-  home.file.".config/rofi/fonts.rasi".source = ./hyprland/rofi/fonts.rasi;
-  home.file.".config/rofi/onedark.rasi".source = ./hyprland/rofi/onedark.rasi;
-
-  ## waybar
-  home.file.".config/waybar/config.jsonc".source = ./hyprland/waybar/config.jsonc;
-  home.file.".config/waybar/style.css".source = ./hyprland/waybar/style.css;
-  home.file.".config/waybar/onedark.css".source = ./hyprland/waybar/onedark.css;
-
   # Kvantum
   home.file.".config/Kvantum/kvantum.kvconfig".source = ./hyprland/kvantum/kvantum.kvconfig;
   home.file.".config/Kvantum/Orchis".source = ./hyprland/kvantum/Orchis;
   ## widget config files 
   # home.file.".config/ags/config.js".source = ./hyprland/ags/config.js;
-
-  home.file.".config/quickshell".source = ./hyprland/quickshell;
 
   #noctalia
   # home.file. ".config/noctalia".source = ./hyprland/noctalia;
---
+++ modules/desktop/hyprland/var.lua
@@ -0,0 +1,10 @@
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+mainMod = "SUPER" -- Sets "Windows" key as main modifier
+
+-- Set programs that you use
+terminal    = "kitty"
+fileManager = "nemo"
+menu        = "rofi -show drun -theme ~/.config/rofi/config.rasi"
