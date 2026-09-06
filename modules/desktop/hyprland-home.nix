cf007484d5af8f9bc593318fafd0445756365174
javi <java@javamurray.com>
2026-09-06 00:35:19 -0700

    fix a typo

--- modules/desktop/hyprland-home.nix
+++ modules/desktop/hyprland-home.nix
@@ -10,7 +10,7 @@
   home.file.".config/kitty/kitty.conf".source = ./kitty/kitty.conf;
   wayland.windowManager.hyprland.systemd.enable = false;
 
-  home.file.".config/Kvantum".source = ./hyprland/Kvantum;
+  home.file.".config/Kvantum".source = ./kvantum;
 
   # swayosd needs its own service for volume/brightness OSD popups
   services.swayosd.enable = true;
