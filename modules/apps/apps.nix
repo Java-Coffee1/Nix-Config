57544e06c0aed080ed52b5ef6b26ffe856487927
javi <java@javamurray.com>
2026-09-20 18:49:52 -0700

    move vs code to a new file

--- modules/apps/apps.nix
+++ modules/apps/apps.nix
@@ -6,46 +6,6 @@
 # You can use https://search.nixos.org/ to find more packages (and options).
 { pkgs, ... }:
 
-let
-  vscode-configured = pkgs.vscode-with-extensions.override {
-    vscodeExtensions =
-      with pkgs.vscode-extensions;
-      [
-        jnoortheen.nix-ide
-        ms-python.python
-        ms-azuretools.vscode-docker
-        ms-vscode-remote.remote-ssh
-        ms-vscode.cpptools
-        platformio.platformio-vscode-ide
-        anthropic.claude-code
-        redhat.java
-        vscjava.vscode-java-debug
-        vscjava.vscode-java-test
-        vscjava.vscode-java-dependency
-      ]
-      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "remote-ssh-edit";
-          publisher = "ms-vscode-remote";
-          version = "0.47.2";
-          sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-        }
-        {
-          name = "hackatime-time-tracker";
-          publisher = "hackatime";
-          version = "30.2.2004";
-          sha256 = "sha256-bX8egMLHdyUq6yWF+ta7jGMBu4NO3KFJtvaA9jfZc/0=";
-        }
-        {
-          name = "remote-explorer";
-          publisher = "ms-vscode";
-          version = "0.6.2026031809";
-          # sha256 = pkgs.lib.fakeSha256;
-          sha256 = "sha256-WRg8ObPVQMluuCW/dBM2ibBDW/zc8cQQS5QQMfcpw2c=";
-        }
-      ];
-  };
-in
 {
   services.flatpak.enable = true;
   environment.systemPackages = with pkgs; [
@@ -80,25 +40,6 @@ in
     easyeffects
     orca-slicer
     reaper
-
-    # -- Development --
-    vscode-configured
-    claude-code
-    tea
-    gcc
-    gnumake
-    python3
-    platformio
     # docker
   ];
-  # -- PlatformIO --
-  programs.nix-ld.enable = true;
-  services.udev.packages = [ pkgs.platformio-core.udev ];
-  hjem.users.javi.files.".config/Code/User/settings.json".text = builtins.toJSON {
-    "editor.fontSize" = 18;
-    "editor.fontFamily" = "'GeistMono Nerd Font Mono', monospace";
-    "terminal.integrated.fontSize" = 14;
-    "window.zoomLevel" = 1;
-    "platformio-ide.useBuiltinPython" = false;
-  };
 }
