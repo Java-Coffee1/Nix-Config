9b725ef69c783c33878be5315d6f340340f6f9c5
javi <java@javamurray.com>
2026-09-22 21:53:35 -0700

    added vim

--- home/javi/home.nix
+++ home/javi/home.nix
@@ -29,81 +29,90 @@
 
     packages = [ pkgs.bibata-cursors ];
 
-    files = lib.mkIf config.javi.isGui {
-      ## Hyprland configuration files
-      ".config/hypr/hyprland.lua".source = ./dotfiles/hyprland/hyprland.lua;
-      ".config/hypr/keybindings.lua".source = ./dotfiles/hyprland/keybindings.lua;
-      ".config/hypr/var.lua".source = ./dotfiles/hyprland/var.lua;
-      ".config/hypr/windows_and_workspaces.lua".source = ./dotfiles/hyprland/windows_and_workspaces.lua;
+    files = lib.mkMerge [
+      ## Always linked (GUI and non-GUI)
+      {
+        ## Vim
+        ".vimrc".source = ./dotfiles/vim/vimrc;
+        ".vim".source = ./dotfiles/vim;
+      }
 
-      ## Wallaper
-      ".config/hypr/wallpaper1.png".source = inputs.wallpapers;
-      "wallpapers".source = inputs.wallpapers;
-      ## Kitty
-      ".config/kitty/kitty.conf".source = ./dotfiles/kitty/kitty.conf;
+      ## GUI only
+      (lib.mkIf config.javi.isGui {
+        ## Hyprland configuration files
+        ".config/hypr/hyprland.lua".source = ./dotfiles/hyprland/hyprland.lua;
+        ".config/hypr/keybindings.lua".source = ./dotfiles/hyprland/keybindings.lua;
+        ".config/hypr/var.lua".source = ./dotfiles/hyprland/var.lua;
+        ".config/hypr/windows_and_workspaces.lua".source = ./dotfiles/hyprland/windows_and_workspaces.lua;
 
-      ## Kvantum
-      ".config/Kvantum".source = ./dotfiles/kvantum;
+        ## Wallaper
+        ".config/hypr/wallpaper1.png".source = inputs.wallpapers;
+        "wallpapers".source = inputs.wallpapers;
+        ## Kitty
+        ".config/kitty/kitty.conf".source = ./dotfiles/kitty/kitty.conf;
 
-      ## cursor theme
-      ".icons/default/index.theme".text = ''
-        [Icon Theme]
-        Name=Default
-        Inherits=Bibata-Modern-Classic
-      '';
+        ## Kvantum
+        ".config/Kvantum".source = ./dotfiles/kvantum;
 
-      ".config/gtk-3.0/settings.ini".text = ''
-        [Settings]
-        gtk-cursor-theme-name=Bibata-Modern-Classic
-        gtk-cursor-theme-size=24
-      '';
+        ## cursor theme
+        ".icons/default/index.theme".text = ''
+          [Icon Theme]
+          Name=Default
+          Inherits=Bibata-Modern-Classic
+        '';
 
-      ## qt theming
-      ".config/qt5ct/qt5ct.conf" = {
-        generator = (pkgs.formats.ini { }).generate "qt5ct.conf";
-        value = {
-          Appearance = {
-            style = "kvantum";
-            icon_theme = "Papirus-Dark";
-            custom_palette = false;
-            standard_dialogs = "default";
+        ".config/gtk-3.0/settings.ini".text = ''
+          [Settings]
+          gtk-cursor-theme-name=Bibata-Modern-Classic
+          gtk-cursor-theme-size=24
+        '';
+
+        ## qt theming
+        ".config/qt5ct/qt5ct.conf" = {
+          generator = (pkgs.formats.ini { }).generate "qt5ct.conf";
+          value = {
+            Appearance = {
+              style = "kvantum";
+              icon_theme = "Papirus-Dark";
+              custom_palette = false;
+              standard_dialogs = "default";
+            };
           };
         };
-      };
 
-      ".config/qt6ct/qt6ct.conf" = {
-        generator = (pkgs.formats.ini { }).generate "qt6ct.conf";
-        value = {
-          Appearance = {
-            style = "kvantum";
-            icon_theme = "Papirus-Dark";
-            custom_palette = false;
-            standard_dialogs = "default";
+        ".config/qt6ct/qt6ct.conf" = {
+          generator = (pkgs.formats.ini { }).generate "qt6ct.conf";
+          value = {
+            Appearance = {
+              style = "kvantum";
+              icon_theme = "Papirus-Dark";
+              custom_palette = false;
+              standard_dialogs = "default";
+            };
           };
         };
-      };
 
-      ## hypridle
-      ".config/hypr/hypridle.conf".text = ''
-        general {
-            lock_cmd = noctalia msg session lock
-            before_sleep_cmd = noctalia msg session lock
-            after_sleep_cmd = hyprctl dispatch dpms on
-        }
+        ## hypridle
+        ".config/hypr/hypridle.conf".text = ''
+          general {
+              lock_cmd = noctalia msg session lock
+              before_sleep_cmd = noctalia msg session lock
+              after_sleep_cmd = hyprctl dispatch dpms on
+          }
 
-        listener {
-            timeout = 600
-            on-timeout = hyprctl dispatch dpms off
-            on-resume = hyprctl dispatch dpms on
-        }
+          listener {
+              timeout = 600
+              on-timeout = hyprctl dispatch dpms off
+              on-resume = hyprctl dispatch dpms on
+          }
 
-        listener {
-            timeout = 900
-            on-timeout = systemctl suspend
-        }
-      '';
-    }; # end of files
-
+          listener {
+              timeout = 900
+              on-timeout = systemctl suspend
+          }
+        '';
+      }) # end of gui files
+    ]; # end of files
     systemd.services.hypridle = {
       description = "Hypridle idle daemon";
       wantedBy = [ "graphical-session.target" ];
@@ -125,5 +134,5 @@
         Restart = "on-failure";
       };
     };
-  };
+  }; # end of hjem users
 }
